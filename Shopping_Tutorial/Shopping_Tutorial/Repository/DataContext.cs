b6c286c3857f8838aa20490eac4e532c2270ac97
thaithanhdat011 <thaithanhdatqn2019@gmail.com>
2025-05-07 22:29:36 +0700

    Seeding Database

---
+++ Shopping_Tutorial/Shopping_Tutorial/Repository/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping_Tutorial.Models;
+
+namespace Shopping_Tutorial.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<BrandModel> Brands { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.ProductModel>().ToTable("Products");
+            modelBuilder.Entity<Models.BrandModel>().ToTable("Brands");
+            modelBuilder.Entity<Models.CategoryModel>().ToTable("Categories");
+        }*/
+    }
+}
