76be21106accdda2d9a8689d735fb64f28ffb4d8
thaithanhdat011 <thaithanhdatqn2019@gmail.com>
2025-05-12 17:10:54 +0700

    cart

---
+++ Shopping_Tutorial/Shopping_Tutorial/Controllers/BrandController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Tutorial.Models;
+using Shopping_Tutorial.Repository;
+
+namespace Shopping_Tutorial.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public BrandController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IActionResult> Index(string Slug = "")
+        {
+            BrandModel brand = _dataContext.Brands.Where(c => c.Slug == Slug).FirstOrDefault();
+            if (brand == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var brandsByCategory = _dataContext.Products.Where(p => p.BrandId == brand.Id);
+            return View(await brandsByCategory.OrderByDescending(p => p.Id).ToListAsync());
+        }
+    }
+}
--- Shopping_Tutorial/Shopping_Tutorial/Controllers/HomeController.cs
+++ Shopping_Tutorial/Shopping_Tutorial/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Shopping_Tutorial.Models;
+using Shopping_Tutorial.Repository;
 
 namespace Shopping_Tutorial.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataContext _dataContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _dataContext = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _dataContext.Products.Include("Category").Include("Brand").ToList();  
+            return View(products);
         }
 
         public IActionResult Privacy()
